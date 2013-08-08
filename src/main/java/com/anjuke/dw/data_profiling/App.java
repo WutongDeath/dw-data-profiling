d3c0967c5ca32bb3b442cead829c81e123ea51f0
Ji Zhang <jizhang@anjuke.com>
2013-08-08 20:01:08 +0800

    numeric stats

--- src/main/java/com/anjuke/dw/data_profiling/App.java
+++ src/main/java/com/anjuke/dw/data_profiling/App.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.json.simple.JSONValue;
@@ -13,10 +15,12 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
-//        Connection connStats = DriverManager.getConnection("jdbc:mysql://localhost/dwms_db?useUnicode=true&characterEncoding=UTF-8", "dwms_db", "dwms_db");
+        Connection connStats = DriverManager.getConnection("jdbc:mysql://localhost/dwms_db?useUnicode=true&characterEncoding=UTF-8", "dwms_db", "dwms_db");
+        PreparedStatement stmtStats = connStats.prepareStatement("UPDATE dp_column SET stats = ? WHERE table_id = 1 AND name = 'toward'");
+
         Connection connStage = DriverManager.getConnection("jdbc:mysql://10.20.8.39:3306/dw_stage?useUnicode=true&characterEncoding=UTF-8", "readonly_v2", "aNjuKe9dx1Pdw");
 
-        PreparedStatement stmt = connStage.prepareStatement("SELECT MIN(comm_id), MAX(comm_id), AVG(comm_id), STDDEV_POP(comm_id) FROM st_dw_haozu_prop");
+        PreparedStatement stmt = connStage.prepareStatement("SELECT MIN(toward), MAX(toward), AVG(toward), STDDEV_POP(toward) FROM st_dw_haozu_prop");
         ResultSet result = stmt.executeQuery();
         result.next();
 
@@ -26,8 +30,23 @@ public class App {
         stats.put("avg", result.getDouble(3));
         stats.put("sd", result.getDouble(4));
 
+        stmt.close();
+        stmt = connStage.prepareStatement("SELECT toward, COUNT(*) FROM st_dw_haozu_prop GROUP BY toward ORDER BY COUNT(*) DESC LIMIT 10");
+        result = stmt.executeQuery();
+
+        List<Object> top10 = new ArrayList<Object>();
+        while (result.next()) {
+            top10.add(result.getObject(1));
+            top10.add(result.getLong(2));
+        }
+        stats.put("top10", top10);
+
         String statsString = JSONValue.toJSONString(stats);
         System.out.println(statsString);
+
+        stmtStats.setString(1, statsString);
+        int affectedRows = stmtStats.executeUpdate();
+        System.out.println("Affected rows: " + String.valueOf(affectedRows));
 
     }
 
